02148f87892d496a00628810644f579bf8c53eae
gkdl4239 <gkdl4239@gmail.com>
2025-01-27 09:51:24 +0900

    feat : 도커파일 작성

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.example.expert.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.example.expert.domain.auth.exception.AuthException;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
@@ -8,14 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<Map<String, Object>> invalidRequestExceptionException(InvalidRequestException ex) {
+    public ResponseEntity<Map<String, Object>> invalidRequestExceptionException(
+        InvalidRequestException ex) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         return getErrorResponse(status, ex.getMessage());
     }
@@ -32,7 +32,8 @@ public class GlobalExceptionHandler {
         return getErrorResponse(status, ex.getMessage());
     }
 
-    public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
+    private ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status,
+        String message) {
         Map<String, Object> errorResponse = new HashMap<>();
         errorResponse.put("status", status.name());
         errorResponse.put("code", status.value());
